0a6ebef611fdfcb8508ad924fa8165d21def7e03
Ritam Kabiraj <56652226+THE-CORE-BUG@users.noreply.github.com>
2021-07-03 23:26:25 +0530

    Update Navbar.js

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,9 +1,25 @@
-import React from "react";
+import React,{ useState,useEffect } from "react";
 import "../styles/navbar.css";
 import logo from "../images/kisspng-catering-food-computer-icons-logo-event-management-catering-5abf487cd18447.7546290415224853728582.png";
 function Navbar() {
+  
+  const [navbar,setNavbar] = useState(false)
+  
+  const changeBackground = () => {
+    if(windows.scrollY >= 100)
+    {
+      setNavbar(true)
+    }
+    else
+    {
+      setNavbar(false)
+    }
+  }
+  
+  windows.addEventListener('scroll',changeBackground);
+  
   return (
-    <header>
+    <header className={navbar? 'navbar active': 'navbar'} >
       <div className="logo">
         <img src={logo} alt="" srcset="" />
         <div>Desi Cafe</div>
